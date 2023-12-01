46be13a2241eb65bc60f6a008fa6a083c9e20ed6
Jéssica Vitoriano <103972128+imJessicaV@users.noreply.github.com>
2023-12-01 12:44:09 -0300

    Continuação

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Random dice = new();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+Random dice = new Random();
+int roll = dice.Next();
