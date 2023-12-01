460c7ae12b88ede502dc7976958e8cc97fe44989
Jéssica Vitoriano <103972128+imJessicaV@users.noreply.github.com>
2023-12-01 15:50:01 -0300

    Exercício – Retornar valores e parâmetros de entrada de métodos

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390af27d15ef71d132935a341a4fa4f442c95b20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46be13a2241eb65bc60f6a008fa6a083c9e20ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
